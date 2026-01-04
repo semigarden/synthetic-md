380b44523b1def9c59c542f5868637ee39f72370
semigarden <semigardenn@gmail.com>
2026-01-04 10:38:52 +0400

    dev mode setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import { useEffect, useState } from 'react'
 import { loadText, saveText } from './db'
 import SyntheticText from './react/SyntheticText'
+import './styles/index.scss'
 
 const App = () => {
-  const [value, setValue] = useState('')
+    const [value, setValue] = useState('')
 
-  useEffect(() => {
-    loadText().then(setValue)
-  }, [])
+    useEffect(() => {
+        loadText().then(setValue)
+    }, [])
 
-  const onChange = (e: Event) => {
-    const target = e.target as HTMLTextAreaElement
-    const v = target.value
-    setValue(v)
-    saveText(v)
-  }
+    const onChange = (e: Event) => {
+        const target = e.target as HTMLTextAreaElement
+        const v = target.value
+        setValue(v)
+        saveText(v)
+    }
 
-  return (
-    <SyntheticText value={value} onChange={onChange} />
-  )
+    return (
+        <SyntheticText className="syntheticText" value={value} onChange={onChange} />
+    )
 }
 
 export default App
