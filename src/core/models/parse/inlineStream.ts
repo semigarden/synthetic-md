bf9545f40903381256137412f6c2cd1c2814e752
semigarden <semigardenn@gmail.com>
2026-01-07 14:03:09 +0400

    add inlineStream

---
+++ src/core/models/parse/inlineStream.ts
@@ -0,0 +1,78 @@
+
+
+class InlineStream {
+    private input: string
+    private currentPosition = 0
+
+    constructor(input: string) {
+        this.input = input
+    }
+
+    public position(): number {
+        return this.currentPosition
+    }
+
+    public end(): boolean {
+        return this.currentPosition >= this.input.length
+    }
+
+    public peek(offset = 0): string | null {
+        const i = this.currentPosition + offset
+        return i < this.input.length ? this.input[i] : null
+    }
+
+    public next(): string | null {
+        if (this.end()) return null
+        return this.input[this.currentPosition++]
+    }
+
+    public consume(char: string): boolean {
+        if (this.peek() === char) {
+            this.currentPosition++
+            return true
+        }
+        return false
+    }
+
+    public consumeWhile(predicate: (ch: string) => boolean): string {
+        const start = this.currentPosition
+        while (!this.end() && predicate(this.peek()!)) {
+            this.currentPosition++
+        }
+        return this.input.slice(start, this.currentPosition)
+    }
+
+    public readUntil(predicate: (ch: string) => boolean): string {
+        const start = this.currentPosition
+        while (!this.end() && !predicate(this.peek()!)) {
+            this.currentPosition++
+        }
+        return this.input.slice(start, this.currentPosition)
+    }
+
+    public match(value: string): boolean {
+        return this.input.startsWith(value, this.currentPosition)
+    }
+
+    public consumeString(value: string): boolean {
+        if (this.match(value)) {
+            this.currentPosition += value.length
+            return true
+        }
+        return false
+    }
+
+    public checkpoint(): number {
+        return this.currentPosition
+    }
+
+    public restore(position: number) {
+        this.currentPosition = position
+    }
+
+    public remaining(): string {
+        return this.input.slice(this.currentPosition)
+    }
+}
+
+export default InlineStream
