af60705862a67737c47b05c66d1db7dd3890abc3
semigarden <semigardenn@gmail.com>
2026-01-11 14:47:06 +0400

    improve editor

--- src/core/models/editor.ts
+++ src/core/models/editor.ts
@@ -2,9 +2,27 @@ import Ast from './ast/ast'
 import Caret from './caret'
 import Render from './render'
 import Timeline from './timeline'
-import { EditEffect, AstApplyEffect } from '../types'
+import { EditEffect, AstEffect, AstApplyEffect } from '../types'
 
 class Editor {
+    private executors: Record<AstEffect['type'], (effect: any) => AstApplyEffect | null> = {
+        input: (effect) => this.ast.input(effect.blockId, effect.inlineId, effect.text, effect.caretPosition),
+        splitBlock: (effect) => this.ast.split(effect.blockId, effect.inlineId, effect.caretPosition),
+        splitListItem: (effect) => this.ast.splitListItem(effect.listItemId, effect.blockId, effect.inlineId, effect.caretPosition),
+        mergeInline: (effect) => this.ast.mergeInline(effect.leftInlineId, effect.rightInlineId),
+        indentListItem: (effect) => this.ast.indentListItem(effect.listItemId),
+        outdentListItem: (effect) => this.ast.outdentListItem(effect.listItemId),
+        mergeTableCell: (effect) => this.ast.mergeTableCell(effect.cellId),
+        addTableColumn: (effect) => this.ast.addTableColumn(effect.cellId),
+        addTableRow: (effect) => this.ast.addTableRow(effect.cellId),
+        addTableRowAbove: (effect) => this.ast.addTableRowAbove(effect.cellId),
+        splitTableCell: (effect) => this.ast.splitTableCell(effect.cellId, effect.blockId, effect.inlineId, effect.caretPosition),
+        splitTableCellAtCaret: (effect) => this.ast.splitTableCellAtCaret(effect.cellId, effect.blockId, effect.inlineId, effect.caretPosition),
+        mergeBlocksInCell: (effect) => this.ast.mergeBlocksInCell(effect.cellId, effect.blockId),
+        mergeInlineInCell: (effect) => this.ast.mergeInlineInCell(effect.cellId, effect.leftInlineId, effect.rightInlineId),
+        insertParagraphAboveTable: (effect) => this.ast.insertParagraphAboveTable(effect.tableId),
+        insertParagraphBelowTable: (effect) => this.ast.insertParagraphBelowTable(effect.tableId),
+    }
     public emitChange: () => void
     public timeline: Timeline
 
@@ -18,15 +36,7 @@ class Editor {
         this.timeline = new Timeline(this, this.snapshot())
     }
 
-    public undo() {
-        this.timeline.undo()
-    }
-
-    public redo() {
-        this.timeline.redo()
-    }
-
-    public snapshot() {
+    private snapshot() {
         return {
             text: this.ast.text,
             blocks: this.ast.blocks,
@@ -39,73 +49,35 @@ class Editor {
         }
     }
 
+    public undo() {
+        this.timeline.undo()
+    }
+
+    public redo() {
+        this.timeline.redo()
+    }
+
     public apply(effect: EditEffect) {
         this.timeline.push(this.snapshot())
-        
-        if (effect.ast) {
-            effect.ast.forEach(effect => {
-                let result: AstApplyEffect | null = null
-                switch (effect.type) {
-                    case 'input':
-                        result = this.ast.input(effect.blockId, effect.inlineId, effect.text, effect.caretPosition)
-                        break
-                    case 'splitBlock':
-                        result = this.ast.split(effect.blockId, effect.inlineId, effect.caretPosition)
-                        break
-                    case 'splitListItem':
-                        result = this.ast.splitListItem(effect.listItemId, effect.blockId, effect.inlineId, effect.caretPosition)
-                        break
-                    case 'mergeInline':
-                        result = this.ast.mergeInline(effect.leftInlineId, effect.rightInlineId)
-                        break
-                    case 'indentListItem':
-                        result = this.ast.indentListItem(effect.listItemId)
-                        break
-                    case 'outdentListItem':
-                        result = this.ast.outdentListItem(effect.listItemId)
-                        break
-                    case 'mergeTableCell':
-                        result = this.ast.mergeTableCell(effect.cellId)
-                        break
-                    case 'addTableColumn':
-                        result = this.ast.addTableColumn(effect.cellId)
-                        break
-                    case 'addTableRow':
-                        result = this.ast.addTableRow(effect.cellId)
-                        break
-                    case 'addTableRowAbove':
-                        result = this.ast.addTableRowAbove(effect.cellId)
-                        break
-                    case 'splitTableCell':
-                        result = this.ast.splitTableCell(effect.cellId, effect.blockId, effect.inlineId, effect.caretPosition)
-                        break
-                    case 'splitTableCellAtCaret':
-                        result = this.ast.splitTableCellAtCaret(effect.cellId, effect.blockId, effect.inlineId, effect.caretPosition)
-                        break
-                    case 'mergeBlocksInCell':
-                        result = this.ast.mergeBlocksInCell(effect.cellId, effect.blockId)
-                        break
-                    case 'mergeInlineInCell':
-                        result = this.ast.mergeInlineInCell(effect.cellId, effect.leftInlineId, effect.rightInlineId)
-                        break
-                    case 'insertParagraphAboveTable':
-                        result = this.ast.insertParagraphAboveTable(effect.tableId)
-                        break
-                    case 'insertParagraphBelowTable':
-                        result = this.ast.insertParagraphBelowTable(effect.tableId)
-                        break
-                }
-                if (!result) return
 
-                const { renderEffect, caretEffect } = result
+        if (!effect.ast) return
 
-                this.ast.normalize()
-                this.render.apply(renderEffect)
-                this.caret.apply(caretEffect)
-                this.emitChange()
-            })
-            this.timeline.updateEvent(this.snapshot())
+        for (const astEffect of effect.ast) {
+            const execute = this.executors[astEffect.type]
+            if (!execute) continue
+
+            const result = execute(astEffect)
+            if (!result) continue
+
+            const { renderEffect, caretEffect } = result
+
+            this.ast.normalize()
+            this.render.apply(renderEffect)
+            this.caret.apply(caretEffect)
+            this.emitChange()
         }
+        
+        this.timeline.updateEvent(this.snapshot())
     }
 }
 
